9817b2c5f05ded5d56b033af2f4424d89d4634f7
Jay Sanghani <sanghanijay6353@gmail.com>
2023-06-10 09:46:43 +0530

    Create Ball by Ball Section

--- src/Cric/Cric.jsx
+++ src/Cric/Cric.jsx
@@ -5,13 +5,7 @@ import teamdata from "./Team.json";
 
 const Cric = () => {
   const totalOver = 20;
-  const Extras = [
-    { name: "Wide", run: 1 },
-    { name: "No Ball", run: 1 },
-    { name: "Bye", run: 0 },
-    { name: "Leg Bye", run: 0 },
-    { name: "Wicket", run: 0 },
-  ];
+  const Extras = [{ name: "WD", run: 1 },{ name: "NB", run: 1 },{ name: "Bye", run: 0 },{ name: "LB", run: 0 },{ name: "OUT", run: 0 }];
   const [extra, setExtra] = useState({ name: "", run: 0 });
   const [data, setData] = useState(teamdata);
   const [strike, setStrike] = useState({
@@ -20,19 +14,13 @@ const Cric = () => {
   });
   const [bowler, setBowler] = useState(teamdata[1].players[6]);
   const [value, setValue] = useState(false);
-  const [oveer, setOveer] = useState(0);
-  const [curRun, setCurRun] = useState(
-    [
-        [
-            // { over: 3, run: 0 }
-        ]
-    ]
-    );
+  const [oveer, setOveer] = useState(0.0);
+  const [curRun, setCurRun] = useState([[]]);
   // const [curData,setCurData] = useState([{run:"",bowlerName:"",batsmanName:"",}])
 
   useEffect(() => {
     let overs = Math.floor(data[1].team_balls / 6);
-    let remainballs = ((data[1].team_balls % 6)+1) / 10;
+    let remainballs = ((data[1].team_balls % 6) ) / 10;
     setOveer(overs + remainballs);
   }, [data]);
   const handleRunClick = (e) => {
@@ -40,33 +28,27 @@ const Cric = () => {
     // setCurRun(newData);
     // console.log(curRun)
     const newData = [...curRun];
-    let curOver = newData[0]
-    const curData = {over:oveer,run:e,bowlerName:bowler.name,batsmanName:strike.onStrike.name}
-    curOver = [curData,...curOver]
-    newData[0] = curOver
+    let curOver = newData[0];
+    const curData = { over: oveer, run: e, bowlerName: bowler.name, batsmanName: strike.onStrike.name };
+    curOver = [curData, ...curOver];
+    newData[0] = curOver;
     setCurRun(newData);
 
     let tempData = { ...data };
-    if (
-      bowler.bowling.balls % 6 == 0 &&
-      bowler.bowling.balls != 0 &&
-      strike.onStrike.batting.runs == 0
-    )
-      bowler.bowling.maiden_over += 1;
+    // if (bowler.bowling.balls % 6 === 0 && bowler.bowling.balls !== 0 && strike.onStrike.batting.runs === 0) 
+        // bowler.bowling.maiden_over += 1;
 
-    if (bowler.bowling.balls % 6 == 0 && bowler.bowling.balls != 0) {
+    if (bowler.bowling.balls % 5 === 0 && bowler.bowling.balls !== 0) {
       let bowlerIndex = parseInt(prompt("Please Bowler Index"));
       while (bowler === tempData[1].players[bowlerIndex]) {
-        alert(
-          "Please, Change the Bowler Same Bowler can't bowling in twice in a line."
-        );
+        alert("Please, Change the Bowler Same Bowler can't bowling in twice in a line.");
         bowlerIndex = parseInt(prompt("Please Bowler Index"));
-        setBowler(tempData[1].players[bowlerIndex]);
       }
+      setBowler(tempData[1].players[bowlerIndex]);
     }
-    if (e % 2 == 0) {
-      if (e == 4) strike.onStrike.batting.fours += 1;
-      if (e == 6) strike.onStrike.batting.sixes += 1;
+    if (e % 2 === 0) {
+      if (e === 4) strike.onStrike.batting.fours += 1;
+      if (e === 6) strike.onStrike.batting.sixes += 1;
     } else {
       setStrike((x) => ({ onStrike: x.offStrike, offStrike: x.onStrike }));
     }
@@ -75,7 +57,7 @@ const Cric = () => {
     bowler.bowling.balls += 1;
     tempData[1].team_balls += 1;
     console.log(tempData[1].team_balls);
-    if (extra.name === "Bye" || extra.name === "Leg Bye") {
+    if (extra.name === "Bye" || extra.name === "LB") {
       strike.onStrike.batting.runs -= e;
       console.log("object");
     }
@@ -91,7 +73,7 @@ const Cric = () => {
     bowler.bowling.runs += extra.run;
     data[0].team_score += extra.run;
     // console.log(extra.run)
-    if (extra.name === "Wicket") {
+    if (extra.name === "OUT") {
       data[0].team_wickets += 1;
       console.log("w");
     }
@@ -116,9 +98,7 @@ const Cric = () => {
           <div className="mainScoreAreaIMG">
             <div className="mainScoreArea p-4">
               <div className="teamScore d-flex ">
-                <div className="fs-4 fw-semibold me-3 p-2 pt-3">
-                  {teamdata[0].team_name}
-                </div>
+                <div className="fs-4 fw-semibold me-3 p-2 pt-3">{teamdata[0].team_name}</div>
                 <div className="scoreArea p-1">
                   <span className="score fs-1 fw-bold">
                     {data[0].team_score}/{data[0].team_wickets}{" "}
@@ -133,15 +113,13 @@ const Cric = () => {
                 <div className="col batsmanArea border-end">
                   <div className="batsmanName">{strike.onStrike.name}</div>
                   <div className="batsmanScore">
-                    {strike.onStrike.batting.runs} (
-                    {strike.onStrike.batting.balls})
+                    {strike.onStrike.batting.runs} ({strike.onStrike.batting.balls})
                   </div>
                 </div>
                 <div className="col">
                   <div className="batsmanName">{strike.offStrike.name}</div>
                   <div className="batsmanScore">
-                    {strike.offStrike.batting.runs} (
-                    {strike.offStrike.batting.balls})
+                    {strike.offStrike.batting.runs} ({strike.offStrike.batting.balls})
                   </div>
                 </div>
               </div>
@@ -163,57 +141,33 @@ const Cric = () => {
               <div className="row d-flex mt-3 border-top pt-3">
                 <div className="col bowlerName">{bowler.name}</div>
                 <div className="col bowlerScore text-end mx-3">
-                  {Math.floor(bowler.bowling.balls / 6)}.
-                  {bowler.bowling.balls % 6} - {bowler.bowling.maiden_over} -{" "}
-                  {bowler.bowling.runs} - {bowler.bowling.wickets}
+                  {Math.floor(bowler.bowling.balls / 6)}.{bowler.bowling.balls % 6} - {bowler.bowling.maiden_over} - {bowler.bowling.runs} - {bowler.bowling.wickets}
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="d-flex flex-wrap my-4">
-            <div className="runPerBall mx-auto">
+            <div className=" runPerBall mx-auto d-flex flex-wrap my-4">
               {[0, 1, 2, 3, 4, 5, 6, 7].map((run, index) => {
                 return (
-                  <button
-                    onClick={() => handleRunClick(run)}
-                    className={`runBtn runBtn${index}`}
-                    key={index}
-                  >
-                    {run}
-                  </button>
+                        <button onClick={() => handleRunClick(run)} className='runBtn' key={index}>{run}</button>
                 );
-              })}
+              })} 
             </div>
-          </div>
 
-          <div className="extra d-flex flex-wrap justify-content-between p-3">
+          <div className="extra d-flex flex-wrap justify-content-between mb-3">
+         
             {Extras.map((extra, index) => {
               return (
                 <div className="my-1" key={index}>
-                  <input
-                    type="checkbox"
-                    onChange={() => handleExtraClick(extra)}
-                    className="btn-check"
-                    id={`${extra.name}`}
-                  />
-                  <label
-                    className={
-                      "btn " +
-                      (`${extra.name}` === "Wicket"
-                        ? "btn-danger extraBtn"
-                        : "btn-success extraBtn")
-                    }
-                    htmlFor={`${extra.name}`}
-                  >
+                  <input type="checkbox" onChange={() => handleExtraClick(extra)} className="btn-check" id={`${extra.name}`} />
+                  <label className={"btn " + (`${extra.name}` === "OUT" ? "btn-danger extraBtn" : "btn-success extraBtn")} htmlFor={`${extra.name}`}>
                     {extra.name}
                   </label>
                 </div>
               );
             })}
-            <div className="extraBtn text-white fs-5 p-2 btn btn-warning">
-              Undo
-            </div>
+                <button className="btn btn-warning text-white  extraBtn">Undo</button>
           </div>
         </div>
 
@@ -230,28 +184,21 @@ const Cric = () => {
 
           <div className="row rowBG">
             <div className="col-8 overNo text-start fs-5">Over {oveer}</div>
-            <div className="col-2 fs-5 ps-3">
-              <FiPlus />
-            </div>
-            <div className="col-2 ps-3 fs-5">
-              <FiEdit3 />
-            </div>
+            <div className="col-2 fs-5 ps-3"><FiPlus /></div>
+            <div className="col-2 ps-3 fs-5"><FiEdit3 /></div>
           </div>
-          <div className="row">
             {curRun.map((over, index) => {
-                return over.map((r,index) => {
-                    return (
-                    <div className="row fs-5 p-2 text-start" key={index}>
-                        <div className="col-2">{r.over}</div>
-                        <div className="col-2"><div className="runPerBall">{r.run}</div></div>
-                        <div className="col-6 fs-6">{bowler.name}</div>
-                        <div className="col-2 text-end pe-4"><FiEdit3/></div>
-                    </div>
-                    )
-                })
-              
+              return over.map((r, index) => {
+                return (
+                  <div className="row fs-5 p-2 text-start" key={index}>
+                    <div className="col-2">{(r.over + 0.1).toFixed(1)}</div>
+                    <div className="col-2 runPerBall ">{r.run}</div>
+                    <div className="col-7" style={{fontSize:'13px'}}>{r.bowlerName}</div>
+                    <div className="col-1 text-end"><FiEdit3 /></div>
+                  </div>
+                );
+              });
             })}
-          </div>
           {/* {
                 curRun.map((r,index) => {
                     return (
